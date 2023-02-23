e26539c32beefd17f4e97a8c494bf2324ea3a346
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-02-23 22:18:26 +0100

    start dev server + install deps

--- src/lib/webcontainer/index.ts
+++ src/lib/webcontainer/index.ts
@@ -1,29 +1,24 @@
 import { WebContainer } from '@webcontainer/api';
 import { writable } from "svelte/store";
 import { files } from './files';
-
+console.log("starting");
 const webcontainer_instance = await WebContainer.boot();
 webcontainer_instance.mount(files);
+
 const { subscribe, update } = writable({
 	current_file: files.src.directory.routes.directory['+page.svelte'].file.contents,
 	current_path: './src/routes/+page.svelte',
-	logs: [] as string[],
+	iframe_url: './loading',
 });
+const { subscribe: subscribe_logs, update: update_logs } = writable<string[]>([]);
 
 type StoreType = Parameters<typeof subscribe>["0"] extends (value: infer ActualType) => unknown ? ActualType : never;
 
 function merge_state(state: Partial<StoreType>) {
-	update(previous_state => {
-		const new_state = structuredClone(previous_state);
-		Object.keys(new_state).forEach((_key) => {
-			const key = _key as keyof typeof new_state;
-			if (Array.isArray(new_state[key])) {
-				//todo deepmerge
-			}
-		});
-		return new_state;
-	});
+	update(previous_state => ({ ...previous_state, state }));
 }
+
+export const logs = { subscribe: subscribe_logs };
 
 export const webcontainer = {
 	subscribe,
@@ -38,11 +33,16 @@ export const webcontainer = {
 		const process = await webcontainer_instance.spawn("npm", ["install"]);
 		process.output.pipeTo(new WritableStream({
 			write(data) {
-				merge_state({
-					logs: []
-				});
+				update_logs(prev => [...prev, data]);
 			}
 		}));
+		return process.exit;
+	},
+	async run_dev_server() {
+		webcontainer_instance.spawn("npm", ["run", "dev"]);
+		webcontainer_instance.on("server-ready", (port, url) => {
+			merge_state({ iframe_url: url });
+		});
 	}
 };
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,22 @@
 <script lang="ts">
-	import { webcontainer } from '$lib/webcontainer';
+	import { webcontainer, logs } from '$lib/webcontainer';
+	import { onMount } from 'svelte';
+	onMount(async () => {
+		debugger;
+		// await webcontainer.install_dependencies();
+		// webcontainer.run_dev_server();
+	});
 </script>
 
-<textarea
+test
+
+<!-- <textarea
 	value={$webcontainer.current_file}
 	on:input={(e) => {
 		webcontainer.update_file($webcontainer.current_path, e.currentTarget.value);
 	}}
 />
-<iframe title="content" />
+<iframe title="content" src={$webcontainer.iframe_url} />
+{#each $logs as log}
+	<div>{log}</div>
+{/each} -->
