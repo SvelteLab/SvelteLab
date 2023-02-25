a8d4c01f7102af470a2ac8e9563791825eb5a009
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-02-25 16:10:58 +0100

    use site-kit instead and use color-scheme

--- src/routes/(inited_webcontainer)/+layout.svelte
+++ src/routes/(inited_webcontainer)/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { webcontainer } from '$lib/webcontainer';
 	import { onMount } from 'svelte';
+	import '@sveltejs/site-kit/styles/index.css';
 	import '../../styles/global.css';
 	onMount(() => {
 		//this is to interact with the filesistem
