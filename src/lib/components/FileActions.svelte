9343da73d263a274225c35cac665caa6ae4b613d
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-02-27 17:32:07 +0100

    Add ability to run and stop scripts

---
+++ src/lib/components/FileActions.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Pane, Splitpanes } from 'svelte-splitpanes';
+	import FileTree from './FileTree.svelte';
+	import RunScripts from './RunScripts.svelte';
+</script>
+
+<Splitpanes horizontal>
+	<Pane size={70}>
+		<FileTree />
+	</Pane>
+	<Pane>
+		<RunScripts />
+	</Pane>
+</Splitpanes>
