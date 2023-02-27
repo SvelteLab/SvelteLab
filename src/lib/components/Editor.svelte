986041046c2d3369cbbb49fec600214c9b914ada
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-02-27 17:54:30 +0100

    add more lang support

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -1,12 +1,28 @@
 <script lang="ts">
 	import { webcontainer } from '$lib/webcontainer';
+	import { css } from '@codemirror/lang-css';
+	import { html } from '@codemirror/lang-html';
+	import { javascript } from '@codemirror/lang-javascript';
+	import { json } from '@codemirror/lang-json';
+	import { markdown } from '@codemirror/lang-markdown';
 	import { svelte } from '@replit/codemirror-lang-svelte';
-
 	import CodeMirror from 'svelte-codemirror-editor';
+
+	const langs: Record<string, ReturnType<typeof svelte>> = {
+		svelte: svelte(),
+		html: html(),
+		js: javascript(),
+		ts: javascript({ typescript: true }),
+		css: css(),
+		json: json(),
+		md: markdown()
+	};
+
+	$: lang = langs[$webcontainer.current_path.split('.').at(-1) || 'svelte'];
 </script>
 
 <CodeMirror
-	lang={svelte()}
+	{lang}
 	value={$webcontainer.current_file}
 	on:change={(e) => {
 		webcontainer.update_file($webcontainer.current_path, e.detail);
--- src/lib/utils/icons.ts
+++ src/lib/utils/icons.ts
@@ -1,17 +1,24 @@
 import File from '~icons/akar-icons/file';
-import Ts from '~icons/vscode-icons/file-type-typescript-official';
-import Js from '~icons/vscode-icons/file-type-js-official';
+import JS from '~icons/vscode-icons/file-type-js-official';
 import Svelte from '~icons/vscode-icons/file-type-svelte';
+import TS from '~icons/vscode-icons/file-type-typescript-official';
 import Vite from '~icons/vscode-icons/file-type-vite';
+import CSS from '~icons/vscode-icons/file-type-css';
+import JSON from '~icons/vscode-icons/file-type-json';
+import HTML from '~icons/vscode-icons/file-type-html';
 
 const icon_maps = new Map<RegExp, typeof File>([
 	[/vite\.config\.(ts|js)$/, Vite],
 	[/svelte\.config\.(ts|js)$/, Svelte],
 	[/.*\.svelte$/, Svelte],
-	[/.*\.ts$/, Ts],
-	[/.*\.js$/, Js],
-	[/.*/, File],
+	[/.*\.ts$/, TS],
+	[/.*\.js$/, JS],
+	[/.*\.css$/, CSS],
+	[/.*\.json$/, JSON],
+	[/.*\.html$/, HTML],
+	[/.*/, File]
 ]);
+
 export function get_icon(filename: string) {
 	for (const [regex, component] of icon_maps.entries()) {
 		if (regex.test(filename)) {
