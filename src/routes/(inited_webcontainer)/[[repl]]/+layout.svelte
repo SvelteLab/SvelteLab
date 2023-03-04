d4994dd4bfa163b2def57e64d4081689921f557f
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-03-04 19:07:05 +0100

    add hash share + fix some watchmode errors

--- src/routes/(inited_webcontainer)/[[repl]]/+layout.svelte
+++ src/routes/(inited_webcontainer)/[[repl]]/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { SvelteToast } from '@zerodevx/svelte-toast';
 	import { webcontainer } from '$lib/webcontainer';
 	import { onMount } from 'svelte';
 	import Booting from '~icons/line-md/loading-alt-loop';
@@ -20,6 +21,13 @@
 		<span> Booting up webcontainer... </span>
 	</div>
 {/await}
+<SvelteToast
+	options={{
+		theme: {
+			'--toastBarBackground': 'var(--sk-theme-1)'
+		}
+	}}
+/>
 
 <style>
 	div {
@@ -27,5 +35,13 @@
 		inset: 0;
 		backdrop-filter: blur(10px);
 		z-index: 9999;
+	}
+	:global(:root) {
+		--toastContainerBottom: 1.5rem;
+		--toastContainerTop: auto;
+		--toastMsgPadding: 1.5rem;
+		--toastWidth: 20rem;
+		--toastBackground: var(--sk-back-2);
+		--toastColor: var(--sk-text-1);
 	}
 </style>
