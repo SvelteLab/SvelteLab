9de35f1d9bdb3d4cf7aa51afdb203fbb513f5007
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-03-07 15:35:07 +0100

    Added zod for validation (#53)

--- src/routes/save-repl/+server.ts
+++ src/routes/save-repl/+server.ts
@@ -1,22 +1,30 @@
+import { replSchema } from '$lib/schemas';
 import { json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ locals, request }) => {
 	const { id, files, name } = await request.json();
+	let to_save;
+	try {
+		to_save = replSchema.parse({ id, files, name, user: locals.user?.id });
+	} catch (e) {
+		return new Response('Parsing error', {
+			status: 500
+		});
+	}
 	const replCollection = locals.poket_base.collection('repls');
 	let created;
 	try {
-		if (id) {
+		if (to_save.id) {
+			// id or user should not be updated
 			await replCollection.update(id.toString(), {
-				files,
-				name
+				files: to_save.files,
+				name: to_save.name
 			});
 		} else {
-			created = await replCollection.create({
-				files,
-				user: locals.user?.id,
-				name
-			});
+			// delete to_save.id just to be sure
+			delete to_save.id;
+			created = await replCollection.create(to_save);
 		}
 	} catch (e) {
 		console.log(e);
