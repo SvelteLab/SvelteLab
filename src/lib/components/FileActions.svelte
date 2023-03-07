c4ed8d431cf0ffaf0bfe9c4088fb778bf0d51af6
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-03-07 20:31:06 +0100

    min size

--- src/lib/components/FileActions.svelte
+++ src/lib/components/FileActions.svelte
@@ -2,13 +2,14 @@
 	import { Pane, Splitpanes } from 'svelte-splitpanes';
 	import FileTree from './FileTree.svelte';
 	import RunScripts from './RunScripts.svelte';
+	export let minSize: number;
 </script>
 
 <Splitpanes horizontal>
-	<Pane size={70}>
+	<Pane size={70} {minSize}>
 		<FileTree />
 	</Pane>
-	<Pane>
+	<Pane {minSize}>
 		<RunScripts />
 	</Pane>
 </Splitpanes>
