84e0edbff8fe6bc1bcc4e11dd8f2efbd2e50cc28
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-03-08 09:57:12 +0100

    added cards with tree map to the profile page

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -7,18 +7,6 @@ export const load: PageServerLoad = async ({ locals }) => {
 	if (!locals.user) {
 		throw redirect(301, '/');
 	}
-<<<<<<< HEAD
-=======
-	const replsCollection = await await locals.poket_base.collection('repls').getFullList();
-	const repls: z.infer<typeof replSchema>[] = [];
-	for (const repl of replsCollection) {
-		try {
-			repls.push(replSchema.parse(repl));
-		} catch (e) {
-			/* no need to throw we just exclude the repl if it's damaged */
-		}
-	}
->>>>>>> e0e57d2... added cards with tree map to the profile page
 	return {
 		repls: locals.pocketbase
 			.collection('repls')
