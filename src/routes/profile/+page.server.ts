b76b1f39d34a3ee4a59f2ed8a9a8cd1d94b847c1
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-03-08 09:57:12 +0100

    added cards with tree map to the profile page

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,10 +1,24 @@
+import { replSchema } from '$lib/schemas';
 import { redirect } from '@sveltejs/kit';
+import type { z } from 'zod';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	if (!locals.user) {
 		throw redirect(301, '/');
 	}
+<<<<<<< HEAD
+=======
+	const replsCollection = await await locals.poket_base.collection('repls').getFullList();
+	const repls: z.infer<typeof replSchema>[] = [];
+	for (const repl of replsCollection) {
+		try {
+			repls.push(replSchema.parse(repl));
+		} catch (e) {
+			/* no need to throw we just exclude the repl if it's damaged */
+		}
+	}
+>>>>>>> e0e57d2... added cards with tree map to the profile page
 	return {
 		repls: locals.pocketbase
 			.collection('repls')
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,9 +1,86 @@
 <script lang="ts">
+	import TreeMap from '$lib/components/TreeMap.svelte';
 	import type { PageData } from './$types';
+	import Share from '~icons/akar-icons/network';
+	import TrashCan from '~icons/akar-icons/trash-can';
+	import { flip } from 'svelte/animate';
 
 	export let data: PageData;
 </script>
 
-{#each data.repls as repl (repl.id)}
-	<a href={repl.id}>{repl.name || repl.id}</a><br />
-{/each}
+<main>
+	{#each data.repls as project (project.id)}
+		<!-- this will be useful when we will add delete -->
+		<a animate:flip href={project.id}>
+			<article>
+				<p>
+					{project.name}
+				</p>
+				<small>/{project.id}</small>
+				<div class="buttons">
+					<button>
+						<TrashCan />
+					</button>
+					<button>
+						<Share />
+					</button>
+				</div>
+				<div class="tree">
+					<TreeMap tree={project.files} />
+				</div>
+			</article>
+		</a>
+	{/each}
+</main>
+
+<style>
+	main {
+		padding: 2rem;
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(30rem, 1fr));
+		gap: 3rem;
+		max-width: 100%;
+	}
+	a {
+		color: var(--sk-text-1);
+		text-decoration: none;
+	}
+	article {
+		box-shadow: 0 4px 6px -1px rgb(0 0 0 / 30%), 0 2px 4px -2px rgb(0 0 0 / 70%);
+		padding: 2rem;
+		position: relative;
+		border-radius: 0.5rem;
+		overflow: hidden;
+	}
+	article > * {
+		max-width: 80%;
+	}
+	.tree {
+		right: 0;
+		top: 0;
+		width: 20%;
+		bottom: 0;
+		position: absolute;
+		border-left: 1px solid var(--sk-code-bg);
+		background: linear-gradient(-45deg, var(--sk-code-bg), transparent);
+	}
+	.tree :global(svg) {
+		position: absolute;
+		height: 100%;
+		width: 100%;
+		object-fit: contain;
+	}
+	.buttons {
+		margin-block: 1rem;
+		display: flex;
+	}
+	p {
+		margin: 0;
+		font-size: 2rem;
+	}
+	small {
+		background: var(--sk-back-2);
+		padding: 0.25rem 0.5rem;
+		grid-row: -1;
+	}
+</style>
