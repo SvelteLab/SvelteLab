bdb40ad8b5e83d537bd6a71430fd1e681109f609
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-03-08 09:57:12 +0100

    added cards with tree map to the profile page

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,13 +1,21 @@
+import { replSchema } from '$lib/schemas';
 import { redirect } from '@sveltejs/kit';
+import type { z } from 'zod';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	if (!locals.user) {
 		throw redirect(301, '/');
 	}
-	const repls = await (
-		await locals.poket_base.collection('repls').getFullList()
-	).map((repl) => structuredClone(repl));
+	const replsCollection = await await locals.poket_base.collection('repls').getFullList();
+	const repls: z.infer<typeof replSchema>[] = [];
+	for (const repl of replsCollection) {
+		try {
+			repls.push(replSchema.parse(repl));
+		} catch (e) {
+			/* no need to throw we just exclude the repl if it's damaged */
+		}
+	}
 	return {
 		repls
 	};
