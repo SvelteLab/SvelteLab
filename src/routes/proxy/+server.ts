128d210e88cd21ded56f2e25b3b0af21de89d031
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-03-11 00:30:18 +0100

    bikeshed commend

--- src/routes/proxy/+server.ts
+++ src/routes/proxy/+server.ts
@@ -1,10 +1,12 @@
 import type { RequestHandler } from './$types';
 /**
+ * With this proxy we can easily pass an url
+ * and it will append the correct CORS header.
+ *
  * We need this because to use webcontainer api we need to
- * have a Cross-Origin-Embedder-Policy but than to embed
- * somethign we need to that request the header Cross-Origin-Resource-Policy
- * set to cross-origin. With this proxy we can easily pass an url
- * and it will append the correct header
+ * have a Cross-Origin-Embedder-Policy, but than to embed
+ * something we need to that request the header
+ * Cross-Origin-Resource-Policy set to cross-origin.
  */
 export const GET: RequestHandler = async ({ url, fetch }) => {
 	const url_to_fetch = url.searchParams.get('url');
