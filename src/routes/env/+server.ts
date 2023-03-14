2c798423b209f503d959d9ae0989d7f98ad80f70
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-03-14 11:27:00 +0100

    test process.env

--- src/routes/env/+server.ts
+++ src/routes/env/+server.ts
@@ -2,5 +2,5 @@ import type { RequestHandler } from './$types';
 import { json } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async () => {
-	return json(import.meta.env);
+	return json(process.env);
 };
