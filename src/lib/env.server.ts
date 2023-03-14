738a89ce3ccb5d54fdd4e35b6870c4ebcc625836
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-03-14 11:58:48 +0100

    Ok this time is for real

--- src/lib/env.server.ts
+++ src/lib/env.server.ts
@@ -2,8 +2,8 @@ import { PUBLIC_GITHUB_REDIRECT_URI } from '$env/static/public';
 
 const IS_PREVIEW = process.env.VERCEL_ENV === 'preview';
 export const REDIRECT_URI = IS_PREVIEW
-	? `https://${process.env.VERCEL_GIT_REPO_SLUG}-${process.env.VERCEL_GIT_COMMIT_REF?.replace(
+	? `https://${process.env.VERCEL_GIT_REPO_SLUG}-git-${process.env.VERCEL_GIT_COMMIT_REF?.replace(
 			'#',
 			''
-	  )}-svelteblitz`
+	  )}-svelteblitz.vercel-app/redirect`
 	: PUBLIC_GITHUB_REDIRECT_URI;
