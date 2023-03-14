550847791c9a38d04854220ac7444d9617f31762
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-03-14 11:38:33 +0100

    fix vercel deploys

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,4 +1,9 @@
 import { PUBLIC_GITHUB_REDIRECT_URI } from '$env/static/public';
 
-const IS_PREVIEW = import.meta.env.VERCEL_ENV === 'preview';
-export const REDIRECT_URI = IS_PREVIEW ? import.meta.env.VERCEL_URL : PUBLIC_GITHUB_REDIRECT_URI;
+const IS_PREVIEW = process.env.VERCEL_ENV === 'preview';
+export const REDIRECT_URI = IS_PREVIEW
+	? `https://${process.env.VERCEL_GIT_REPO_SLUG}-${process.env.VERCEL_GIT_COMMIT_REF?.replace(
+			'#',
+			''
+	  )}-svelteblitz`
+	: PUBLIC_GITHUB_REDIRECT_URI;
