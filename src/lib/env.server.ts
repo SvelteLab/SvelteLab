ec2140f0481e149f899db3eaf6ca4807fd1b54e0
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-03-14 12:00:47 +0100

    Argh, typo!

--- src/lib/env.server.ts
+++ src/lib/env.server.ts
@@ -5,5 +5,5 @@ export const REDIRECT_URI = IS_PREVIEW
 	? `https://${process.env.VERCEL_GIT_REPO_SLUG}-git-${process.env.VERCEL_GIT_COMMIT_REF?.replace(
 			'#',
 			''
-	  )}-svelteblitz.vercel-app/redirect`
+	  )}-svelteblitz.vercel.app/redirect`
 	: PUBLIC_GITHUB_REDIRECT_URI;
