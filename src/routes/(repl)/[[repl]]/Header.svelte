f959605f211bc88a48bbb3173817a4a9b2596f15
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-03-27 01:25:36 +0200

    equal radii

--- src/routes/(repl)/[[repl]]/Header.svelte
+++ src/routes/(repl)/[[repl]]/Header.svelte
@@ -324,10 +324,12 @@
 		right: 0;
 		box-shadow: 0 4px 6px -1px rgb(0 0 0 / 20%), 0 2px 4px -2px rgb(0 0 0 / 20%);
 		z-index: 10;
+		border-bottom-left-radius: 0.5em;
+		border-bottom-right-radius: 0.5em;
 	}
 	ul > :last-child {
-		border-bottom-left-radius: 0.2em;
-		border-bottom-right-radius: 0.2em;
+		border-bottom-left-radius: 0.5em;
+		border-bottom-right-radius: 0.5em;
 		overflow: hidden;
 	}
 	li :is(button, a) {
