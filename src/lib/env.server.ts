0b49818294a84753656590e2ae600dff56429dd0
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-04-09 11:42:05 +0200

    fix redirect uri for hackaton.sveltelab.dev

--- src/lib/env.server.ts
+++ src/lib/env.server.ts
@@ -1,6 +1,8 @@
 import { PUBLIC_GITHUB_REDIRECT_URI } from '$env/static/public';
 
-const IS_PREVIEW = process.env.VERCEL_ENV === 'preview';
+// on vercel this is setupped to replace_me for every preview branch
+// except hackaton.sveltelab.dev
+const IS_PREVIEW = PUBLIC_GITHUB_REDIRECT_URI === 'replace_me';
 export const REDIRECT_URI = IS_PREVIEW
 	? `https://${process.env.VERCEL_GIT_REPO_SLUG}-git-${process.env.VERCEL_GIT_COMMIT_REF?.replace(
 			'#',
