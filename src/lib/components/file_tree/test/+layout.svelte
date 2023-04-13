6ed1e36298c29bfe56e8f9a3b3a3384dbc246a69
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-04-13 13:02:35 +0200

    remove test folder

--- src/lib/components/file_tree/test/+layout.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import type { LayoutData } from './$types';
-
-	export let data: LayoutData;
-</script>
--- src/lib/components/file_tree/test/+server.ts
+++
@@ -1,5 +0,0 @@
-import type { RequestHandler } from './$types';
-
-export const GET: RequestHandler = async () => {
-	return new Response();
-};
