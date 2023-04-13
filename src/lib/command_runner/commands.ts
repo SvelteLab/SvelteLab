06f2664b7f7a8feb01d8e842359c2f8a6b82cb6f
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-04-13 18:08:14 +0200

    update share wording

--- src/lib/command_runner/commands.ts
+++ src/lib/command_runner/commands.ts
@@ -300,7 +300,7 @@ export const commands: Readable<Command[]> = derived([files, page], ([$files, $p
 	if ($page.data.id) {
 		commands_to_return.push({
 			command: 'share-id',
-			title: 'Share via id',
+			title: 'Share Project (via id)',
 			subtitle: 'copy link that shares current project via id',
 			icon: Share,
 			async action() {
@@ -316,8 +316,8 @@ export const commands: Readable<Command[]> = derived([files, page], ([$files, $p
 
 	commands_to_return.push({
 		command: 'share-hash',
-		title: 'Share via Hash',
-		subtitle: 'copy link that shares current project via hash',
+		title: 'Share Files (via Hash)',
+		subtitle: 'copy link that shares current files via hash',
 		icon: Share,
 		async action() {
 			const progress_toast = toast.push(`Sharing...`, {
