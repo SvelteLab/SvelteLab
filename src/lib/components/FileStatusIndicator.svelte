e52b8fc4c4ee5b09a652423b15d74248a039386b
K-Sato1995 <katsuki_sato0130@icloud.com>
2023-05-15 09:51:26 +0900

    chore!: refactor FileStatusIndicator

--- src/lib/components/FileStatusIndicator.svelte
+++ src/lib/components/FileStatusIndicator.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
 	import { file_status } from '$lib/stores/repl_id_store';
 	export let path = '';
-	export let file_name = '';
 
 	let is_edited = file_status.is_file_edited(path);
 </script>
 
-{file_name}{#if $is_edited}*{/if}
+{#if $is_edited}*{/if}
