f4a55db36b29dc97fd3a4fdaa750756eb5c51a25
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-06-29 19:31:28 +0200

    add validation

--- src/routes/(auth)/register/+page.server.ts
+++ src/routes/(auth)/register/+page.server.ts
@@ -1,11 +1,19 @@
 import { fail, redirect, type Actions } from '@sveltejs/kit';
+import { z } from 'zod';
+
+const register_schema = z.object({
+	email: z.string(),
+	password: z.string(),
+	passwordConfirm: z.string(),
+	name: z.string().optional(),
+});
 
 export const actions = {
 	default: async ({ request, locals }) => {
 		const data = Object.fromEntries(await request.formData());
-		console.log(data);
 
 		try {
+			register_schema.parse(data);
 			await locals.pocketbase.collection('users').create(data);
 			await locals.pocketbase
 				.collection('users')
