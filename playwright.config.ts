35ffc734875f9e8350a7581f0ab00cbc3fd5398b
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-07-10 23:53:15 +0200

    fixup webServer

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,13 +3,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 const dev = process.env.PLAYWRIGHT_DEV;
 
+console.log('######asdfa');
+
 //if we ever want /utils
 // → https://playwright.dev/docs/test-configuration#filtering-tests
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: dev ? '' : 'npm run build && npm run preview',
 		port: dev ? 5173 : 4173,
-		reuseExistingServer: dev,
+		reuseExistingServer: !!dev,
 	},
 	testDir: './tests',
 	testMatch: '*.ts',
