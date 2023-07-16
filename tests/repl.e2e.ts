54c930b140d8cea220861e47a81d738c1a27199b
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-07-16 15:25:20 +0200

    swag?

--- tests/repl.e2e.ts
+++ tests/repl.e2e.ts
@@ -1,14 +1,17 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 test.describe('REPL', () => {
 	test.beforeEach(async ({ page }) => {
 		await page.goto('/');
+		expect(
+			page.frameLocator('iframe[title="content"]').getByText('Hello Basic Template!')
+		).toBeVisible({ timeout: 60000 });
 	});
 	test('reflects changes', async ({ page }) => {
 		await page.getByText('<script>').click(); // select Editor
-		await page.keyboard.down('Meta');
+		await page.keyboard.down('Control');
 		await page.keyboard.press('A');
-		await page.keyboard.up('Meta');
+		await page.keyboard.up('Control');
 		await page.keyboard.press('Backspace');
 		const test_string = 'Hello World';
 		await page.keyboard.type('Hello World');
