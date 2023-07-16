221d750cb25c659bd6b1a82461823f6b93623757
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-07-16 15:05:23 +0200

    add correct path

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,15 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
+	testDir: 'tests',
+	testMatch: /.*\.e2e\.ts/,
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173,
 	},
-	testDir: 'tests',
-	testMatch: /.*\.e2e\.ts/,
+	use: {
+		video: 'retain-on-failure',
+	},
 };
 
 export default config;
