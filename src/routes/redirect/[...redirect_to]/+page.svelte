afc8785833a309884d8401071895d04ee9136b8d
paoloricciuti <ricciutipaolo@gmail.com>
2023-07-21 17:55:46 +0200

    better login page

--- src/routes/redirect/[...redirect_to]/+page.svelte
+++ src/routes/redirect/[...redirect_to]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import LogoLong from '$lib/components/LogoLong.svelte';
 	import { onMount } from 'svelte';
 
 	export let data;
@@ -20,4 +21,23 @@
 	});
 </script>
 
-Logged in
+<main>
+	<LogoLong />
+	<h1>You've been logged in...if this page doesn't close automatically you can close it.</h1>
+</main>
+
+<style>
+	:global(body) {
+		background-color: var(--sk-back-1);
+		display: grid;
+		place-items: center;
+	}
+	main {
+		display: grid;
+		place-items: center;
+		gap: 1rem;
+	}
+	h1 {
+		font-size: 3rem;
+	}
+</style>
