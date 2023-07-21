1e20aa06ba4d76be6a274c8450c642f33bfead73
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-07-21 16:14:38 +0200

    update redirect uri to allow cli to work

---
+++ src/routes/redirect/[...redirect_to]/+page.server.ts
@@ -0,0 +1,35 @@
+import { GITHUB_VERIFIER_COOKIE_NAME } from '$env/static/private';
+import { REDIRECT_URI } from '$lib/env.server.js';
+import { redirect } from '@sveltejs/kit';
+
+export async function load({ locals, url, cookies, params: { redirect_to } }) {
+	const code = url.searchParams.get('code');
+	const port = url.searchParams.get('port');
+	const code_verifier = cookies.get(GITHUB_VERIFIER_COOKIE_NAME);
+	if (port) {
+		return {
+			port,
+			code,
+		};
+	}
+	let auth_data;
+	try {
+		auth_data = await locals.pocketbase
+			.collection('users')
+			.authWithOAuth2Code('github', code ?? '', code_verifier ?? '', REDIRECT_URI);
+	} catch (e) {
+		console.log('error', e);
+	}
+	if (auth_data?.meta) {
+		const { name, avatarUrl: avatar_url } = auth_data.meta;
+		try {
+			await locals.pocketbase.collection('users').update(auth_data.record.id, {
+				name,
+				avatarUrl: avatar_url,
+			});
+		} catch (e) {
+			console.log(e);
+		}
+	}
+	throw redirect(302, `/${redirect_to}?login=true`);
+}
---
+++ src/routes/redirect/[...redirect_to]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let data;
+
+	onMount(() => {
+		fetch(`http://localhost:${data.port}`, {
+			body: JSON.stringify({ code: data.code }),
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then(() => {
+				window.close();
+			})
+			.catch(() => {
+				window.close();
+			});
+	});
+</script>
+
+Logged in
