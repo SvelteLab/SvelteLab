28404ff672005619fb66066a414eda7335e6b1cb
Josh Nussbaum <joshnuss@gmail.com>
2023-07-21 19:32:36 -0400

    Adds svelte-preprocess to openprops template

--- src/lib/default_project_files/openprops_postcss/svelte.config.js
+++ src/lib/default_project_files/openprops_postcss/svelte.config.js
@@ -1,7 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess({ postcss: true }),
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
