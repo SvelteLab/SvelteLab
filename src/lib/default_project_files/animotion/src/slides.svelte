0c47ca2a00f3248d514edf85a11cfb90a08de5f1
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-07-25 00:25:06 +0200

    animotion

--- src/lib/default_project_files/animotion/src/slides.svelte
+++ src/lib/default_project_files/animotion/src/slides.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	import { Presentation, Slide } from '@components'
+	import { Presentation, Slide, Code } from '@components'
 </script>
 
 <Presentation>
-	<Slide>
-		<p class="font-bold">🪄 Animotion</p>
+	<Slide animate>
+		<p class="font-bold text-6xl">🪄 Animotion</p>
+	</Slide>
+	<Slide animate>
+		<p class="font-bold text-6xl">🪄 Animotion</p>
+		<p class="font-bold mt-3">in {`{🧪}`} SvelteLab!</p>
 	</Slide>
 </Presentation>
