b5a1461eab24c1c316fda00b846928a876c0e896
snuffy <72365477+snuffyDev@users.noreply.github.com>
2023-07-28 08:43:39 -0400

    reintroduce temp `files` object

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -43,6 +43,68 @@
 			this.worker.terminate();
 		}
 	}
+
+	// Regular files
+	const files = {
+		'Comp1.svelte':
+			`<` +
+			'scrip' +
+			`t lang="ts"` +
+			'>' +
+			`
+	function add(a, b) {
+		return a + b;
+	}
+
+	console.log(add(3, 5));
+</script` +
+			'>',
+		'App.svelte':
+			'<' +
+			`scrip` +
+			`t lang="ts">
+	import { onMount } from 'svelte';
+
+	let photos: Array<{thumbnailUrl: string, title: string}> = [];
+
+	onMount(async () => {
+		const res = await fetch(\`/tutorial/api/album\`);
+		photos = await res.json();
+	});
+` +
+			'</script' +
+			`>
+
+<h1>Photo album</h1>
+
+<div class="photos">
+	{#each photos as photo}
+		<figure>
+			<img src={photo.thumbnailUrl} alt={photo.title} />
+			<figcaption>{photo.title}</figcaption>
+		</figure>
+	{:else}
+		<!-- this block renders when photos.length === 0 -->
+		<p>loading...</p>
+	{/each}
+</div>
+
+<style>
+	.photos {
+		width: 100%;
+		display: grid;
+		grid-template-columns: repeat(5, 1fr);
+		grid-gap: 8px;
+	}
+
+	figure,
+	img {
+		width: 100%;
+		margin: 0;
+	}
+</style>
+`,
+	};
 </script>
 
 <script lang="ts">
