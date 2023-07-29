dec56337c50992303f67b6ec41920bdbbb4471b8
paoloricciuti <ricciutipaolo@gmail.com>
2023-07-29 16:30:00 +0200

    upgrade deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,8 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier','plugin:svelte/recommended'],
+	plugins: ['@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
@@ -11,12 +11,22 @@ module.exports = {
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
+		extraFileExtension: ['.svelte']
 	},
 	env: {
 		browser: true,
 		es2017: true,
 		node: true,
 	},
+	overrides: [
+		{
+		  files: ['*.svelte'],
+		  parser: 'svelte-eslint-parser',
+		  parserOptions: {
+			parser: '@typescript-eslint/parser'
+		  }
+		}
+	  ],
 	rules: {
 		'@typescript-eslint/naming-convention': [
 			'error',
