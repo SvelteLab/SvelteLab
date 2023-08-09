ce8dd8e265e0070aad34feec91ae0bc72db95f97
paoloricciuti <ricciutipaolo@gmail.com>
2023-08-10 00:22:49 +0200

    try with fresh project

--- src/lib/default_project_files/basic/src/routes/+page.svelte
+++ src/lib/default_project_files/basic/src/routes/+page.svelte
@@ -1,16 +1,2 @@
-<script>
-	import Counter from '$lib/Counter.svelte';
-
-	export let data;
-</script>
-
-<h1>
-	Hello {data.name}!
-</h1>
-
-<Counter />
-
-<p>
-	Learn more about templates at
-	<a href="https://docs.sveltelab.dev/templates" target="_blank"> docs.sveltelab.dev/templates </a>
-</p>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
