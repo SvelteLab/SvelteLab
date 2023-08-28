f17556b24b513ce93df08ac8272718f69e01930a
paoloricciuti <ricciutipaolo@gmail.com>
2023-08-28 15:56:26 +0200

    fix: ssr rewrite crashing server

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -89,3 +89,9 @@ export class MapOfSet<K, V extends Set<TSet>, TSet = unknown> extends Map<K, V> 
 		return super.get(key)!;
 	}
 }
+
+export function inject_string(initial_string: string, index: number, string_to_inject: string) {
+	return `${initial_string.substring(0, index)}${string_to_inject}${initial_string.substring(
+		index,
+	)}`;
+}
