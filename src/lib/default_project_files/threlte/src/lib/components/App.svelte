2d07b568fa22f706f34487c85a570eaa83b56ae6
snuffy <72365477+snuffyDev@users.noreply.github.com>
2023-10-25 22:59:40 -0400

    fix threlte template

--- src/lib/default_project_files/threlte/src/lib/components/App.svelte
+++ src/lib/default_project_files/threlte/src/lib/components/App.svelte
@@ -1,8 +1,8 @@
 <script>
-	import { Canvas } from '@threlte/core';
-	import Scene from './Scene.svelte';
+  import { Canvas } from '@threlte/core'
+  import Scene from './Scene.svelte'
 </script>
 
 <Canvas>
-	<Scene />
+  <Scene />
 </Canvas>
