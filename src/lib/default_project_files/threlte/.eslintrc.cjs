f0a4c8b3623226c4c78b4b49ca7fcf1d23f0d660
paoloricciuti <ricciutipaolo@gmail.com>
2023-12-15 14:26:59 +0100

    update threlte template

--- src/lib/default_project_files/threlte/.eslintrc.cjs
+++ src/lib/default_project_files/threlte/.eslintrc.cjs
@@ -1,6 +1,7 @@
+/** @type { import("eslint").Linter.FlatConfig } */
 module.exports = {
 	root: true,
-	extends: ['eslint:recommended', 'plugin:svelte/recommended', 'prettier'],
+	extends: ['eslint:recommended', 'plugin:svelte/recommended'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
