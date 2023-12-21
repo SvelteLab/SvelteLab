a13b48534a90718918566006620cd7f9953815bd
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-12-22 00:12:53 +0100

    fix typeerror

--- src/routes/[github=github]/[...repo]/+server.ts
+++ src/routes/[github=github]/[...repo]/+server.ts
@@ -2,6 +2,7 @@ import { build_import_href } from '$lib/github_import_regex';
 import { redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async ({ params }) => {
+export const GET: RequestHandler = ({ params }) => {
 	redirect(301, build_import_href(params.repo));
+	return new Response();
 };
