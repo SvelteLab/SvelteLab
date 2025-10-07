883191110495d88495966ce0b14ef5ff55bfd285
paoloricciuti <ricciutipaolo@gmail.com>
2025-10-07 18:44:37 +0200

    feat: enable experimental options by default

--- src/lib/default_project_files/tailwind/svelte.config.js
+++ src/lib/default_project_files/tailwind/svelte.config.js
@@ -7,9 +7,16 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		experimental: {
+			remoteFunctions: true
+		}
 	},
-
+	compilerOptions: {
+		experimental: {
+			async: true
+		}
+	},
 	preprocess: [
 		preprocess({
 			postcss: true
